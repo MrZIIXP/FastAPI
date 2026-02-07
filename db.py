fd8c2e241686979bf6ebf8e2051bfc1791c15652
MrZii <133667338+MrZIIXP@users.noreply.github.com>
2026-02-07 16:21:02 +0500

    fix bugs

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker, declarative_base
-
+load_dotenv()
 DB_url = os.getenv('DATABASE_URL')
 engine = create_engine(DB_url)
 sessionLocal = sessionmaker(engine)
